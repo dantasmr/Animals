92e62e701f8ddff83b84d43ce3ce33ce1898ae42
Marcelo <dantasmr@yahoo.com.br>
2020-02-23 19:46:08 -0300

    implementado Dagger

---
+++ app/src/main/java/com/devtides/animals/di/ViewModelComponent.kt
@@ -0,0 +1,13 @@
+package com.devtides.animals.di
+
+import com.devtides.animals.viewmodel.ListViewModel
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules=[ApiModule::class, PrefsModule::class, AppModule::class])
+interface ViewModelComponent {
+
+fun inject (viewModel : ListViewModel)
+
+}
