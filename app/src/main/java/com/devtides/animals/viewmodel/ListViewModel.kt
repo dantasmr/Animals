f0d315f2cbf32b8e55fa5f15cbb2b08c6a2bd28a
Marcelo <dantasmr@yahoo.com.br>
2020-02-23 12:43:46 -0300

    implementado Retrofit + Glide

--- app/src/main/java/com/devtides/animals/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/devtides/animals/viewmodel/ListViewModel.kt
@@ -4,6 +4,13 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.devtides.animals.model.Animal
+import com.devtides.animals.model.AnimalApiService
+import com.devtides.animals.model.ApiKey
+import com.devtides.animals.util.SharePreferenceHelper
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class ListViewModel (application : Application) : AndroidViewModel(application){
 
@@ -12,25 +19,89 @@ class ListViewModel (application : Application) : AndroidViewModel(application){
     val loadError by lazy {MutableLiveData<Boolean>()}
     val loading by lazy {MutableLiveData<Boolean>()}
 
+    val disposable = CompositeDisposable();
+    val apiService = AnimalApiService();
+    val sharePreferenceHelper = SharePreferenceHelper(getApplication())
+    var invalidApiKey = false
+
 
     fun refresh(){
-        getAnimals()
+        loading.value = true
+        invalidApiKey = false
+        val key = sharePreferenceHelper.getApiKey()
+        if (key.isNullOrEmpty()) {
+            getKey()
+        }else{
+            getAnimals(key)
+        }
     }
 
-    private fun getAnimals(){
+    fun hardRefresh(){
+        loading.value = true
+        getKey()
+    }
 
-        val a1 = Animal("Sapo")
-        val a2 = Animal("Cobra")
-        val a3 = Animal("Rato")
-        val a4 = Animal("Abelha")
-        val a5 = Animal("Cavalo")
-        val a6 = Animal("Gato")
 
-        val animalList = listOf(a1, a2, a3, a4, a5)
-        animals.value = animalList
-        loadError.value = false
-        loading.value = false
+    private fun getKey(){
+        disposable.add(
+            apiService.getKey()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<ApiKey>(){
+                    override fun onSuccess(key: ApiKey) {
+                       if (key.key.isNullOrEmpty()){
+                           loadError.value = true
+                           loading.value = false
+                       }else{
+                           sharePreferenceHelper.saveApiKey(key.key)
+                           getAnimals(key.key)
+                       }
+                    }
 
+                    override fun onError(e: Throwable) {
+                       e.printStackTrace()
+                       loadError.value = true
+                       loading.value = false
+                    }
+
+                })
+        )
+    }
+
+    private fun getAnimals(key : String){
+
+        disposable.add(
+            apiService.getAnimals(key)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<List<Animal>>(){
+                    override fun onSuccess(list: List<Animal>) {
+                        loadError.value = false
+                        loading.value = false
+                        animals.value = list
+
+
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                        if (!invalidApiKey){
+                            invalidApiKey = true
+                            getKey()
+                        }
+                        loadError.value = true
+                        loading.value = false
+                        animals.value = null
+                    }
+
+                })
+        )
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 
 
